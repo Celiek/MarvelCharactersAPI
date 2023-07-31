9a113803f1d9017ba0d09bd971e0dac94e70e1b3
Celiek <bosKowski96@gmail.com>
2023-07-31 17:37:22 +0200

    Initial commit

---
+++ demo/src/test/java/com/example/marvelCharacters/MarvelCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marvelCharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelCharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
